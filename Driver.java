316fc2a1059e34d008e214238b4cae585870aaaa
Adam Atrash <adamatrash@alMacBook-Pro-alkhas-bAdam-2.local>
2025-09-06 10:46:17 +0300

    Add Driver class which runs the program

---
+++ Driver.java
@@ -0,0 +1,13 @@
+
+/**
+ * This class includes a reader for the CSV file and creates the metrolink graph
+ * takes inputs from user and check their validity
+ */
+public class Driver {
+    public static void main(String[] args) {
+        // the path of the CSV data file
+        String filePath = "/Users/adamatrash/Desktop/metrolink_summerproject-main/Metrolink_times_linecolour.csv";
+        GUI userGui = new GUI(filePath);
+        
+    }
+}
